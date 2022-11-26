abd66239988fbaf19a51334d3252daa0203ace70
Felix <cmekusfelix8@gmail.com>
2022-11-26 05:22:19 +0100

    created the UI of create listing page without adding functionalities

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Profile from './pages/Profile'
+import CreateListing from './pages/CreateListing'
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </Router>
 
